4c50e7148e664e53979c33f8f32d366beb8de363
tylers3 <77304920+tylers3@users.noreply.github.com>
2021-02-05 13:15:12 -0800

    Javascript Canvas
    Learned about the canvas and drawing shapes.

--- tsu/_js/myScript.js
+++ tsu/_js/myScript.js
@@ -17,16 +17,71 @@
 // ctx.fillStyle = 'green';
 // ctx.fillRect(10, 10, 150, 100);
 
-function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-  
-      ctx.beginPath();
-      ctx.moveTo(75, 50);
-      ctx.lineTo(100, 75);
-      ctx.lineTo(100, 25);
-      ctx.fill();
-      
-    }
+// let myName = 'Tyler';
+
+// myName = 'John Cena';
+
+// if (myName == 'Jim'){
+//   console.log("Hello Jim");
+// }
+
+// Init a function to draw a triangle on the canvas
+function drawTriangle1() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(100, 100);
+    ctx.lineTo(0, 0);
+    ctx.lineTo(200, 0);
+    ctx.fill();
   }
+}
+
+// Init a function to draw a triangle on the canvas
+function drawTriangle2() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(100, 100);
+    ctx.lineTo(0, 200);
+    ctx.lineTo(200, 200);
+    ctx.fill();
+  }
+}
+
+// Init a function to draw a square on the canvas
+function drawSquare() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.lineTo(200, 200);
+    ctx.lineTo(200, 200);
+    ctx.fill();
+  }
+}
+
+// Init a funtion to draw a circle on the canvas
+// https://www.w3resource.com/javascript-exercises/javascript-drawing-exercise-2.php
+function drawCircle() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.arc(100, 100, 20, 0, 2 * Math.PI, false);
+    // (x,y,radius,0,2)
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#FF0000';
+    ctx.stroke();
+  }
+}
+
+// Puts the shapes on the canvas
+function main() {
+  drawTriangle1();
+  drawTriangle2();
+  drawSquare();
+  drawCircle();
+}
